3081c8ab04408763d245aae00f9c25625e6b6031
Fredrik Svedberg <nemo@nemo-ESPRIMO-C5720.(none)>
2013-03-18 15:51:14 +0100

    Connected zoom, pan and tilt in the UI.

--- camerawindow.cpp
+++ camerawindow.cpp
@@ -4,7 +4,9 @@
 CameraWindow::CameraWindow(QWidget *parent)
     : QMainWindow(parent),
       ui(new Ui::CameraWindow),
-      ttyDev("/dev/ttyS0")
+      ttyDev("/dev/ttyS0"),
+      panSpeed(1),
+      tiltSpeed(1)
 {
     ui->setupUi(this);
     OpenInterface();
@@ -53,4 +55,124 @@ void CameraWindow::CloseInterface()
         fprintf(stderr,"\n");
     }
     VISCA_close_serial(&interface);
+}
+
+void CameraWindow::on_cameraPowerButton_clicked()
+{
+    UInt8_t power = 3;
+//    VISCA_get_power(&interface, &camera, &power);
+    // TODO: Power off (3) seems to wait forever for a response, only do power on (2)
+    VISCA_set_power(&interface, &camera, power == 2 ? 3 : 2);
+}
+
+void CameraWindow::on_cameraAutoOffSpinBox_valueChanged(int arg1)
+{
+
+}
+
+void CameraWindow::on_cameraDisplayCheckBox_stateChanged(int arg1)
+{
+    switch (arg1)
+    {
+    case Qt::Unchecked:
+        VISCA_set_datascreen_off(&interface, &camera);
+        break;
+    case Qt::Checked:
+            VISCA_set_datascreen_on(&interface, &camera);
+        break;
+    }
+}
+
+void CameraWindow::on_cameraIrReceiveCheckBox_stateChanged(int arg1)
+{
+    switch (arg1)
+    {
+    case Qt::Unchecked:
+        VISCA_set_irreceive_off(&interface, &camera);
+        break;
+    case Qt::Checked:
+            VISCA_set_irreceive_on(&interface, &camera);
+        break;
+    }
+
+}
+
+void CameraWindow::on_cameraIrReturnCheckBox_stateChanged(int arg1)
+{
+
+}
+
+void CameraWindow::on_panTiltJoystickUpLeftButton_clicked()
+{
+    VISCA_set_pantilt_upleft(&interface, &camera, panSpeed, tiltSpeed);
+}
+
+void CameraWindow::on_panTiltJoystickUpButton_clicked()
+{
+    VISCA_set_pantilt_up(&interface, &camera, panSpeed, tiltSpeed);
+}
+
+void CameraWindow::on_panTiltJoystickUpRightButton_clicked()
+{
+    VISCA_set_pantilt_upright(&interface, &camera, panSpeed, tiltSpeed);
+}
+
+void CameraWindow::on_panTiltJoystickRightButton_clicked()
+{
+    VISCA_set_pantilt_right(&interface, &camera, panSpeed, tiltSpeed);
+}
+
+void CameraWindow::on_panTiltJoystickDownRightButton_clicked()
+{
+    VISCA_set_pantilt_downright(&interface, &camera, panSpeed, tiltSpeed);
+}
+
+void CameraWindow::on_panTiltJoystickDownButton_clicked()
+{
+    VISCA_set_pantilt_down(&interface, &camera, panSpeed, tiltSpeed);
+}
+
+void CameraWindow::on_panTiltJoystickDownLeftButton_clicked()
+{
+    VISCA_set_pantilt_downleft(&interface, &camera, panSpeed, tiltSpeed);
+}
+
+void CameraWindow::on_panTiltJoystickLeftButton_clicked()
+{
+    VISCA_set_pantilt_left(&interface, &camera, panSpeed, tiltSpeed);
+}
+
+void CameraWindow::on_panTiltJoystickStopButton_clicked()
+{
+    VISCA_set_pantilt_stop(&interface, &camera, panSpeed, tiltSpeed);
+}
+
+void CameraWindow::on_panTiltHomeButton_clicked()
+{
+    VISCA_set_pantilt_home(&interface, &camera);
+}
+
+void CameraWindow::on_panTiltResetButton_clicked()
+{
+    VISCA_set_pantilt_reset(&interface, &camera);
+}
+
+void CameraWindow::on_zoomSlider_valueChanged(int value)
+{
+    VISCA_set_zoom_value(&interface, &camera, value);
+}
+
+void CameraWindow::on_zoomTeleButton_clicked()
+{
+    VISCA_set_zoom_tele(&interface, &camera);
+}
+
+void CameraWindow::on_zoomStopButton_clicked()
+{
+    VISCA_set_zoom_stop(&interface, &camera);
+}
+
+void CameraWindow::on_zoomWideButton_clicked()
+{
+    VISCA_set_zoom_wide(&interface, &camera);
 }
--- camerawindow.h
+++ camerawindow.h
@@ -16,6 +16,47 @@ public:
     CameraWindow(QWidget *parent = 0);
     ~CameraWindow();
 
+private slots:
+    void on_cameraPowerButton_clicked();
+
+    void on_cameraAutoOffSpinBox_valueChanged(int arg1);
+
+    void on_cameraDisplayCheckBox_stateChanged(int arg1);
+
+    void on_cameraIrReceiveCheckBox_stateChanged(int arg1);
+
+    void on_cameraIrReturnCheckBox_stateChanged(int arg1);
+
+    void on_panTiltJoystickUpLeftButton_clicked();
+
+    void on_panTiltJoystickUpButton_clicked();
+
+    void on_panTiltJoystickUpRightButton_clicked();
+
+    void on_panTiltJoystickRightButton_clicked();
+
+    void on_panTiltJoystickDownRightButton_clicked();
+
+    void on_panTiltJoystickDownButton_clicked();
+
+    void on_panTiltJoystickDownLeftButton_clicked();
+
+    void on_panTiltJoystickLeftButton_clicked();
+
+    void on_panTiltJoystickStopButton_clicked();
+
+    void on_panTiltHomeButton_clicked();
+
+    void on_panTiltResetButton_clicked();
+
+    void on_zoomSlider_valueChanged(int value);
+
+    void on_zoomTeleButton_clicked();
+
+    void on_zoomStopButton_clicked();
+
+    void on_zoomWideButton_clicked();
+
 private:
     void OpenInterface();
     void CloseInterface();
@@ -25,6 +66,9 @@ private:
     VISCAInterface_t interface;
     VISCACamera_t camera;
     char* ttyDev;
+
+    unsigned int panSpeed;
+    unsigned int tiltSpeed;
 };
 
 #endif // CAMERAWINDOW_H
